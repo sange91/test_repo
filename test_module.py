6e6247398180b597c2ab6e2b06b9c342f34ec53e
Sange Lama <slama@localhost.localdomain>
2018-01-20 11:55:29 +0530

    test_branch initial commit.

--- test_module.py
+++ test_module.py
@@ -3,6 +3,11 @@ def test(arg):
     # unwanted but committed
     # another unwanted commit
 
+def test2(arg):
+    print "You have entered : %s" % arg
 
 if __name__ == "__main__":
     test("my_arg")
+    test2("my_arg")
+    
+
